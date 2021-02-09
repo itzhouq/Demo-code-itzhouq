4f3d9024e11c61feb5d23c3c065a34d00bcc3614
itzhouq <itzhouq@163.com>
2021-02-09 20:16:58 +0800

    .feat (.location) :<自定义编写实现类设置登录名和密码>

--- spring-boot-security-demo/spring-security-hello/src/main/java/cn/itzhouq/config/SecurityConfig2.java
+++ spring-boot-security-demo/spring-security-hello/src/main/java/cn/itzhouq/config/SecurityConfig2.java
@@ -1,24 +1,25 @@
 package cn.itzhouq.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author zhouquan
  * @date 2021/2/9 上午11:47
  */
-//@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = passwordEncoder.encode("123");
-        auth.inMemoryAuthentication().withUser("lucy").password(password).roles("admin");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
---
+++ spring-boot-security-demo/spring-security-hello/src/main/java/cn/itzhouq/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package cn.itzhouq.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author zhouquan
+ * @date 2021/2/9 下午7:46
+ */
+@Service("userDetailsService")
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("role");
+        return new User("marry", new BCryptPasswordEncoder().encode("123"), auths);
+    }
+}
